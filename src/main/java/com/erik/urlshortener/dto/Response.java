d44b6d1a7c1eead2a4ddf2f983edab84468e3451
eriksmedeiros <erik.medeiros.701@ufrn.edu.br>
2025-07-21 22:12:17 -0300

    refactor response

--- src/main/java/com/erik/urlshortener/dto/Response.java
+++ src/main/java/com/erik/urlshortener/dto/Response.java
@@ -1,8 +1,4 @@
 package com.erik.urlshortener.dto;
 
 public record Response(String shortUrl, String fullUrl) {
-    public Response(String shortUrl, String fullUrl) {
-        this.shortUrl = shortUrl;
-        this.fullUrl = fullUrl;
-    }
 }
